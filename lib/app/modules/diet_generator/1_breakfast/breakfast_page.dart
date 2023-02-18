be6d17dcf668b2216e3a04727ec308406bcc03c0
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-02-17 21:59:46 -0300

    ajustando tela de breakfast com wrap

--- lib/app/modules/diet_generator/1_breakfast/breakfast_page.dart
+++ lib/app/modules/diet_generator/1_breakfast/breakfast_page.dart
@@ -9,20 +9,38 @@ class BreakfastPage extends GetView<BreakfastController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('breakfast')),
-      body: Obx(() => ListView.builder(
-            itemCount: controller.breakfast.length,
-            itemBuilder: (context, index) => Row(
-              children: controller.breakfast[index]
-                  .map((value) => Row(
-                        children: [
-                          Text('${value.option}'),
-                          Text('${value.amount}'),
-                          Text('${value.grammage}'),
-                        ],
-                      ))
-                  .toList(),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.blue,
+          height: Get.height,
+          width: Get.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Container(
+              color: Colors.white,
+              child: Obx(() => ListView.builder(
+                    itemCount: controller.breakfast.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => Wrap(
+                      children: controller.breakfast[index]
+                          .map((value) =>  IntrinsicWidth(
+                            child: Card(
+                              child: Row(
+                                children: [
+                                  Text('${value.option}, '),
+                                  Text('${value.amount}, '),
+                                  Text('${value.grammage} '),
+                                ],
+                              ),
+                            ),
+                          ))
+                          .toList(),
+                    ),
+                  )),
             ),
-          )),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => controller.addMealBreakfast(),
