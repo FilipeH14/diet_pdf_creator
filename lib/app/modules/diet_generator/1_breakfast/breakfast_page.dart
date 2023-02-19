a72083c216e6ba9810624f455aa463efea4153ba
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-02-18 21:14:46 -0300

    aplicando condição para mostrar dieta

--- lib/app/modules/diet_generator/1_breakfast/breakfast_page.dart
+++ lib/app/modules/diet_generator/1_breakfast/breakfast_page.dart
@@ -10,49 +10,55 @@ class BreakfastPage extends GetView<BreakfastController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DietAppBar(dietTitle: 'Desjejum'),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                'Dieta do desjejum!!',
-                style: TextStyle(
-                  color: Colors.grey[400],
-                  fontSize: 25,
+      body: Visibility(
+        visible: controller.breakfast.isNotEmpty,
+        replacement: const Center(
+          child: Text('Montar o Desjejum'),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  'Dieta do desjejum!!',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 25,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              child: Obx(() => ListView.separated(
-                    itemCount: controller.breakfast.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) => Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Wrap(
-                        children: controller.breakfast[index]
-                            .map((value) => IntrinsicWidth(
-                                  child: Card(
-                                    child: Row(
-                                      children: [
-                                        Text('${value.option}, '),
-                                        Text('${value.amount}, '),
-                                        Text('${value.grammage} '),
-                                      ],
+              Container(
+                color: Colors.white,
+                child: Obx(() => ListView.separated(
+                      itemCount: controller.breakfast.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Wrap(
+                          children: controller.breakfast[index]
+                              .map((value) => IntrinsicWidth(
+                                    child: Card(
+                                      child: Row(
+                                        children: [
+                                          Text('${value.option}, '),
+                                          Text('${value.amount}, '),
+                                          Text('${value.grammage} '),
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                ))
-                            .toList(),
+                                  ))
+                              .toList(),
+                        ),
                       ),
-                    ),
-                    separatorBuilder: (context, index) => Divider(
-                      color: Colors.grey[400],
-                    ),
-                  )),
-            ),
-          ],
+                      separatorBuilder: (context, index) => Divider(
+                        color: Colors.grey[400],
+                      ),
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
