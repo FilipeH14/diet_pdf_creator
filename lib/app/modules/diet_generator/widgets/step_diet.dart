b5dc9b01aed1e7d90641032699bbe3d801f1cf05
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-02-19 17:02:50 -0300

    ajustando os layouts dos steps

--- lib/app/modules/diet_generator/widgets/step_diet.dart
+++ lib/app/modules/diet_generator/widgets/step_diet.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:diet_pdf_creator/app/shared/ui/diet_ui_colors.dart';
 import 'package:flutter/material.dart';
 
 class StepDiet extends StatelessWidget {
@@ -9,10 +12,76 @@ class StepDiet extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: const [
-          Text('Step Diet!!'),
-          Text('Step Diet!!'),
-          Text('Step Diet!!'),
+        children: [
+          CircleAvatar(
+            backgroundColor: DietUiColors.i.midiumGreen,
+            radius: 10,
+            child: Container(
+              height: 10,
+              width: 10,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          CircleAvatar(
+            backgroundColor: DietUiColors.i.midiumGreen,
+            radius: 10,
+            child: Container(
+              height: 10,
+              width: 10,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          CircleAvatar(
+            backgroundColor: DietUiColors.i.midiumGreen,
+            radius: 10,
+            child: Container(
+              height: 10,
+              width: 10,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          CircleAvatar(
+            backgroundColor: DietUiColors.i.midiumGreen,
+            radius: 10,
+            child: Container(
+              height: 10,
+              width: 10,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          CircleAvatar(
+            backgroundColor: DietUiColors.i.midiumGreen,
+            radius: 10,
+            child: Container(
+              height: 10,
+              width: 10,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          CircleAvatar(
+            backgroundColor: DietUiColors.i.midiumGreen,
+            radius: 10,
+            child: const Icon(
+              Icons.check_outlined,
+              color: Colors.white,
+              size: 18,
+            ),
+          ),
         ],
       ),
     );
